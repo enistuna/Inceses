513c80742d5b038d7d7aa97c562aed42e6ca977b
Enis Tuna <enissstuna@gmail.com>
2025-09-21 15:01:40 +0300

    slight tweaks to the documentation

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-# İnceseslem - Turkish Phonetic Transcriptor
+# İnceses - Turkish Phonetic Transcriptor
 
-*  The phonetic transcriptor function was first written for the 'İncesöz' project in 2024 and is now being used for its own Python package for easier deployment and usability.
-The function was first developed using the Özgün Koşaner's lecture notes from the Phonetics class at Dokuz Eylul University's Linguistics department.
+*  The phonetic transcriptor function was first written for the [İncesöz](https://github.com/enistuna/incesoz_turkish_linguistics_analyzer) project in 2024 and is now being used for its own Python package for easier deployment and usability.
+The function was first developed using the **Özgün Koşaner's** lecture notes from the Phonetics class at *Dokuz Eylul University's Linguistics* department.
 
-## Usage ##
-* To use it, simply import the Python package and write the "inceseslem.phonetic_analysis('')" line of code. 
+## Explanation ##
+* To use it, simply import the Python package and write the "inceses.phonetic_analysis('')" line of code. 
 Finally, put the Turkish word you want to transcript in the quotation marks and run the code
 
+## Usage ##
+```py
+import inceses
 
-"""py
-import inceseslem
+decipher = inceses.phonetic_analysis('Enis Tuna ile yemeğe mi gittin?')
 
-print(inceseslem.phonetic_analysis('enis tuna burada mı?'))
-"""
+print(decipher)
+>> "[ ɛnis tʰunα ile jɛme•e mi ɟittʰin? ]"
+```
--- src/inceses/__init__.py
+++ src/inceses/__init__.py
@@ -1,20 +1,34 @@
-## İnceseslem - Turkish Phonetic Transcriptor
+## İnceses - Turkish Phonetic Transcriptor
 ## by: Enis Tuna
 
 """
-# İnceseslem - Turkish Phonetic Transcriptor #
-*  The phonetic transcriptor function was first written for the 'İncesöz' project in 2024 and is now being used for its own Python package for easier deployment and usability.
-The function was first developed using the Özgün Koşaner's lecture notes from the Phonetics class at Dokuz Eylul University's Linguistics department.
+## İnceses - Turkish Phonetic Transcriptor
 
-## Usage ##
-* To use it, simply import the Python package and write the "inceseslem.phonetic_analysis('')" line of code. 
+*  The phonetic transcriptor function was first written for the İncesöz project in 2024 and is now being used for its own 
+Python package for easier deployment and usability. The function was first developed using the **Özgün Koşaner's** lecture notes from the Phonetics 
+class at *Dokuz Eylul University's Linguistics* department.
+
+### Explanation
+* To use it, simply import the Python package and write the "inceses.phonetic_analysis('')" line of 
+code. 
 Finally, put the Turkish word you want to transcript in the quotation marks and run the code
+
+### Usage
+```
+import inceses
+
+decipher = inceses.phonetic_analysis('Enis Tuna ile yemeğe mi gittin?')
+
+print(decipher)
+>> "[ ɛnis tʰunα ile jɛme•e mi ɟittʰin? ]"
+```
 """
 
 import re
 
 
 def phonetic_analysis(word):
+    word = word.strip().lower()
 
     phonetic_map = {
         "c": "dʒ",
